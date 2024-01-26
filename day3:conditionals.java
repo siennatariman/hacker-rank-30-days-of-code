2f455a6b997733747384f1550cd3258d31d2103a
siennatariman <tariman.sienna@gmail.com>
2024-01-26 19:05:51 +0800

    accepted soln. for both test cases

--- day3:conditionals.java
+++ day3:conditionals.java
@@ -13,7 +13,16 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
-    
+        
+        if (N%2 != 0){
+            System.out.println("Weird");
+        } else if ((N%2 == 0) && (N<=5) && (N>=2)){
+            System.out.println("Not Weird");
+        } else if ((N%2 == 0) && (N<=20) && (N>=6)){
+            System.out.println("Weird");
+        } else if ((N%2 == 0) && (N>20)){
+            System.out.println("Not Weird");
+        }
 
         bufferedReader.close();
     }

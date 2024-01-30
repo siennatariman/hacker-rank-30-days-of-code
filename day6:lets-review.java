3961bf96049d33ffdbb491694aa32a016f781a60
siennatariman <tariman.sienna@gmail.com>
2024-01-30 18:50:09 +0800

    implemented a while loop so it can take in n number of strings but its only taking in one

--- day6:lets-review.java
+++ day6:lets-review.java
@@ -3,10 +3,41 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    
+    // Create a Scanner object to read input from STDIN  
+    Scanner scanner = new Scanner(System.in);  
+    
+    // Firt input is an integer = number of test cases/strings
+    int numTestCase = scanner.nextInt();
+    // Counter to keep track of the number of strings received
+    int counter = 0;
+    
+    // Keep receiving input until specified num. is reached
+    while(counter < numTestCase){
+        String stringInput = scanner.nextLine();
+        
+        String storeEven = new String();
+        String storeOdd = new String();
+        
+        for(int i=0; i<stringInput.length(); i+=2){
+        storeEven = storeEven + stringInput.charAt(i);
+        }
+        
+        for(int m=1; m<stringInput.length(); m+=2){
+        storeOdd = storeOdd + stringInput.charAt(m);
+        }
+        System.out.println(storeEven + " " + storeOdd);
+        counter++;
+    }
+    
+    // Close the scanner to avoid resource leaks
+    scanner.close();
+    
     }
 }

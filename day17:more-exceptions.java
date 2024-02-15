1e395bdd2ab3eb0461e960b73bed76e237cdaa65
siennatariman <tariman.sienna@gmail.com>
2024-02-15 19:55:09 +0800

    added an if conditional statement to check if n and p are negative, if either n or p is negative, the power method will throw an IllegalArgumentException w/ appropriate error message

--- day17:more-exceptions.java
+++ day17:more-exceptions.java
@@ -4,7 +4,13 @@ import java.io.*;
 //Write your code here
 class Calculator{
     
-    public int power(int n, int p){
+    public int power(int n, int p) throws IllegalArgumentException{
+        
+        if(n<0 || p<0){
+            throw new IllegalArgumentException("n and p should be non-negative");
+        }
+
+
         return n^p;
     }
     

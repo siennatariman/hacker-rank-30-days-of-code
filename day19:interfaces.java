3b258d6f0974499cd8fedb91f7d8c260dc61d09c
siennatariman <tariman.sienna@gmail.com>
2024-02-22 21:42:27 +0800

    accepted solution for all seven test cases

--- day19:interfaces.java
+++ day19:interfaces.java
@@ -7,7 +7,6 @@ interface AdvancedArithmetic{
 class Calculator implements AdvancedArithmetic {
     int sum = 0;
     public int divisorSum(int n) {
-        
         for(int i=1; i <= n;i++){
             if(n%i==0){
                 sum += i;

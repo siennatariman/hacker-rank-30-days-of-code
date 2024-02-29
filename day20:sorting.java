26b52d8ba98fed0990718ca2953fbc5b53807535
siennatariman <tariman.sienna@gmail.com>
2024-02-29 22:36:55 +0800

    added a version of bubble sort

--- day20:sorting.java
+++ day20:sorting.java
@@ -28,6 +28,22 @@ public class Solution {
         }
 
         // Code:
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+    
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    swap(a[j], a[j + 1]);
+                    numberOfSwaps++;
+                }
+            }
+    
+        // If no elements were swapped during a traversal, array is sorted
+        if (numberOfSwaps == 0) {
+            break;
+        }
 
         bufferedReader.close();
     }
